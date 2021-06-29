0da64145e05ebcf96ba9d7e8d5b54c108f7fbc55
Almog Ben‐Yochanan <benyochanan@mail.tau.ac.il>
2021-06-29 18:15:01 +0300

    suppressed warning

---
+++ src/Tester.java
@@ -0,0 +1,21 @@
+import java.util.Random;
+
+/**
+ * 
+ */
+
+/**
+ * @author almog
+ *
+ */
+public class Tester {
+	private static Random rand = new Random();
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println(rand.nextInt());
+	}
+
+}
